bd2f4ea6dc83896a13381d33248d941f59b77638
谭茂林 <1920978559@qq.com>
2023-03-24 13:58:35 +0800

    聊天窗口

---
+++ src/main/java/com/itheima/Application.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version v1.0
+ * @ClassName: Application
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: 黑马程序员
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
